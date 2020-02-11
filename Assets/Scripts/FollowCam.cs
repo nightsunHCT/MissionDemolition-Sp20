6212f2dde55e8a3542f5299ec46894c4ea7e883f
Helena <nightsunHCT@users.noreply.github.com>
2020-02-11 02:32:51 -0500

    FolloCam part 1

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; // the static point of interest
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+
+    }
+
+    void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces 
+        if (POI == null) return; // return if there is no POI
+
+        // Get the position of the poi
+        Vector3 destination = POI.transform.position;
+        // Foce destination.z to be camZ to keep teh camera far enough away
+        destination.z = camZ;
+        // set teh camera to the destination
+        transform.position = destination;
+    }
+}
