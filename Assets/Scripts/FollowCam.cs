dd2cadc0a10c3494d45382f40205f114994aa568
Helena <nightsunHCT@users.noreply.github.com>
2020-02-11 02:57:09 -0500

    follow cam part 2 + zoom out cam

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // the static point of interest
 
+    [Header("Set in Inspector")]
+    public float easing = .05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -22,9 +26,16 @@ public class FollowCam : MonoBehaviour
 
         // Get the position of the poi
         Vector3 destination = POI.transform.position;
-        // Foce destination.z to be camZ to keep teh camera far enough away
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Force destination.z to be camZ to keep teh camera far enough away
         destination.z = camZ;
         // set teh camera to the destination
         transform.position = destination;
+        // Set teh orthographic Size fo the Cmara to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
