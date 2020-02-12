d5f2930c807a065bd8193eff729760f521f52d32
Helena <nightsunHCT@users.noreply.github.com>
2020-02-12 02:52:05 -0500

    Hit the Goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    static public bool goalMet = false;
+
+    void OnTriggerEnter (Collider other)
+    {
+
+        // When the trigger is hit by something 
+        // Check to see if it's a projectile 
+        if (other.gameObject.tag == "Projectile")
+        {
+            // if yes, set goalMet to true
+            Goal.goalMet = true;
+            // Also set the alpha of the color to higher opacity
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+}
